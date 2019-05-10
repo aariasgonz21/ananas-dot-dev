7adfa554926bb15f54dcd8c15b8ff468978e94a3
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2019-05-10 18:03:47 -0400

    toggle

---
+++ src/PieceComponents/Menu.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Menu extends Component {
+
+  render() {
+    return (
+      <div className="menu">
+        <h3>about</h3>
+        <h3>mission</h3>
+        <h3>projects</h3>
+        <h3>the jams</h3>
+        <h3>contact</h3>
+      </div>
+    );
+  }
+
+}
+
+export default Menu;
--- src/PieceComponents/Nav.js
+++ src/PieceComponents/Nav.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import Menu from './Menu'
 
 class Nav extends Component {
+    state = {
+      clicked:true
+    }
+
+  clickHandler = () => {
+    this.setState({
+      clicked: !this.state.clicked
+    })
+    console.log("is this nav clicked? " + this.state.clicked)
+  }
 
   render() {
     return (
-      <div>
-         <h2>nav</h2>
+      <div className="nav">
+         <h2 onClick={this.clickHandler}>nav</h2>
       </div>
     );
   }
