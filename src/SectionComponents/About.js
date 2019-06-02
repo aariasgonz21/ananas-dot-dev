01d26c7be71b158cf82fb1112087e5638764d178
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2019-06-02 17:54:58 -0400

    bold nav

--- src/SectionComponents/About.js
+++ src/SectionComponents/About.js
@@ -10,15 +10,9 @@ function About() {
         <div className="about-bg">
           <img src={aboutimg} className="about-img" alt='ana'/>
           <ul className="about-list">
-            <li className="about-li-item">
-
-            A New York native with Dominican Roots</li>
-            <li className="about-li-item">
-
-            A frontend engineer with an eye for design & an entrepreneurial spirit</li>
-            <li className="about-li-item">
-
-            On the constant pursuit  of intersectional equality </li>
+            <li className="about-li-item">A New York native with Dominican Roots</li>
+            <li className="about-li-item">A frontend engineer with an eye for design & an entrepreneurial spirit</li>
+            <li className="about-li-item">On the constant pursuit  of intersectional equality </li>
             <li className="about-li-item">A lover of animation & all things nerdy</li>
           </ul>
         </div>
