3f81db9e2b81af04c87074590c2eedd7d8c2311d
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2019-06-02 21:10:32 -0400

    about on desktop

--- src/SectionComponents/About.js
+++ src/SectionComponents/About.js
@@ -6,8 +6,7 @@ import star from '../images/star.svg'
 function About() {
   return (
       <div id="about">
-        <h2 className="about-title">Ana is... <img className="star" src={star} alt="star"/></h2>
-        <div className="about-bg">
+        <h2 className="about-title">Ana is...</h2>
           <img src={aboutimg} className="about-img" alt='ana'/>
           <ul className="about-list">
             <li className="about-li-item">A New York native with Dominican Roots</li>
@@ -15,7 +14,6 @@ function About() {
             <li className="about-li-item">On the constant pursuit  of intersectional equality </li>
             <li className="about-li-item">A lover of animation & all things nerdy</li>
           </ul>
-        </div>
       </div>
     );
 }
