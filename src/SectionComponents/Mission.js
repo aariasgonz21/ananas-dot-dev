5fcba694024a0427bfc12dd2d83aa164830f9473
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2019-06-10 19:51:16 -0400

    mission desktop

--- src/SectionComponents/Mission.js
+++ src/SectionComponents/Mission.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import '../stylesheets/mission.scss'
+import divider from '../images/divider.svg'
 
 class Mission extends Component {
 
   render() {
     return (
       <div id="mission">
-        <h2>Mission</h2>
+        <h2 className="mission-title"> My Mission </h2>
+        <h3 className="mission-text"> My goal is to one day inspire youth, promote diversity and and bring more people that look like me into tech. </h3>
+        <img className="divider" src={divider} alt="divider"/>
       </div>
     );
   }
