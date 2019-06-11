bf5e0278eacb33c10674bec2abefd29d3ef448fe
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2019-06-10 20:25:54 -0400

    i need ideas

--- src/SectionComponents/Mission.js
+++ src/SectionComponents/Mission.js
@@ -7,9 +7,11 @@ class Mission extends Component {
   render() {
     return (
       <div id="mission">
-        <h2 className="mission-title"> My Mission </h2>
-        <h3 className="mission-text"> My goal is to one day inspire youth, promote diversity and and bring more people that look like me into tech. </h3>
-        <img className="divider" src={divider} alt="divider"/>
+        <div className="container">
+          <h2 className="mission-title"> My Mission </h2>
+          <img className="divider" src={divider} alt="divider"/>
+          <h3 className="mission-text"> My goal is to one day inspire youth, promote diversity, and bring more people that look like me into tech. </h3>
+        </div>
       </div>
     );
   }
